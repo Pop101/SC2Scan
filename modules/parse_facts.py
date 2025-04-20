e3a3ab10e209fc432a2d63cebb87db08953398e0
Leon Leibmann <leon.leibmann@gmail.com>
2025-04-20 01:54:33 -0700

    Dateparser fix for robustness

--- modules/parse_facts.py
+++ modules/parse_facts.py
@@ -1,11 +1,17 @@
 from modules.factoids import MismatchedGame, LongGame, LongStreak, EloHigh, Promote, SwitchRace, ManyGames, EloClimb
 from modules.search_player import search_player, get_player_history
 from modules.traverse import traverse
-from dateutil.parser import parse
 from itertools import zip_longest
 import datetime
+import dateutil.parser
 import re
 from collections import Counter
+
+def safe_dateparse(date_str:str):
+    try:
+        return dateutil.parser.parse(date_str)
+    except:
+        return datetime.datetime(year=1, month=1, day=1, tzinfo=datetime.timezone.utc)
 
 def parse_player_history(player, history, cutoff_date:datetime.datetime = datetime.datetime(year=1, month=1, day=1, tzinfo=datetime.timezone.utc)):
     """Parses all match history for a player and yields all interesting facts"""
@@ -34,7 +40,7 @@ def parse_player_history(player, history, cutoff_date:datetime.datetime = dateti
     # history['history'] has one entry for each key in HISTORY_KEYS
     # we want to convert this to a list of dicts, where each dict has the keys as keys, and the values as values
     all_hist = []
-    history['history']['dateTime'] = [parse(date) for date in history['history']['dateTime']]
+    history['history']['dateTime'] = [safe_dateparse(date) for date in history['history']['dateTime']]
     values = [history['history'].get(key, []) for key in HISTORY_KEYS]
     for entry in zip_longest(*values, fillvalue=None):
         hist_dict = {key: value for key, value in zip(HISTORY_KEYS, entry)}
@@ -102,7 +108,7 @@ def parse_player_matches(player, matches, cutoff_date:datetime.datetime = dateti
         if traverse(team_one, 'team', 'members') is None : continue
         
         # Ignore games before cutoff date
-        if parse(match["match"]["date"]) < cutoff_date:
+        if safe_dateparse(match["match"]["date"]) < cutoff_date:
             continue
         
         # For now: ignore all non-1v1s
@@ -122,6 +128,7 @@ def parse_player_matches(player, matches, cutoff_date:datetime.datetime = dateti
         curr_division = my_team["team"]["league"]["type"]
         elo           = my_team["team"]["rating"]
         
+        print(f"match found: {my_team['team']['league']['type']} {my_team['team']['rating']} {my_team['participant']['decision']} {my_team['participant']['ratingChange']} {match['match']['date']}")
         if highest_after_lowest == None:
             highest_elo = elo
             lowest_elo = elo
@@ -142,26 +149,27 @@ def parse_player_matches(player, matches, cutoff_date:datetime.datetime = dateti
             else:
                 current_elo += my_team["participant"]["ratingChange"]
         
-        # Event 0: Promote?
-        if max_league == None:
-            max_league = curr_division
-        elif curr_division > max_league:
-            league_switches += 1
-            max_league = curr_division
+        # Event 0: Promote? I don't really understand how divisions work so commented out
+        # print("division", curr_division)
+        # if max_league == None:
+        #     max_league = curr_division
+        # elif curr_division > max_league:
+        #     league_switches += 1
+        #     max_league = curr_division
             
-            yield Promote(
-                timestamp   = parse(match["match"]["date"]),
-                player_id   = player_id,
-                player_name = player_name,
-                battle_tag  = battle_tag,
+        #     yield Promote(
+        #         timestamp   = safe_dateparse(match["match"]["date"]),
+        #         player_id   = player_id,
+        #         player_name = player_name,
+        #         battle_tag  = battle_tag,
                 
-                league = max_league,
-            )
+        #         league = max_league,
+        #     )
         
         # Event 1: Possible Mismatch?
         if traverse(other_team, 'team', 'rating') is not None:
             yield MismatchedGame(
-                timestamp   = parse(match["match"]["date"]),
+                timestamp   = safe_dateparse(match["match"]["date"]),
                 player_id   = player_id,
                 player_name = player_name,
                 battle_tag  = battle_tag,
@@ -174,7 +182,7 @@ def parse_player_matches(player, matches, cutoff_date:datetime.datetime = dateti
         # Event 2: Long Game?
         if traverse(match, 'duration') is not None:
             yield LongGame(
-                timestamp   = parse(match["match"]["date"]),
+                timestamp   = safe_dateparse(match["match"]["date"]),
                 player_id   = player_id,
                 player_name = player_name,
                 battle_tag  = battle_tag,
@@ -188,7 +196,7 @@ def parse_player_matches(player, matches, cutoff_date:datetime.datetime = dateti
                 streak_count += 1
             else:
                 yield LongStreak(
-                    timestamp   = parse(match["match"]["date"]),
+                    timestamp   = safe_dateparse(match["match"]["date"]),
                     player_id   = player_id,
                     player_name = player_name,
                     battle_tag  = battle_tag,
@@ -205,7 +213,7 @@ def parse_player_matches(player, matches, cutoff_date:datetime.datetime = dateti
                 streak_count += 1
             else:
                 yield LongStreak(
-                    timestamp   = parse(match["match"]["date"]),
+                    timestamp   = safe_dateparse(match["match"]["date"]),
                     player_id   = player_id,
                     player_name = player_name,
                     battle_tag  = battle_tag,
@@ -220,7 +228,7 @@ def parse_player_matches(player, matches, cutoff_date:datetime.datetime = dateti
     # Clean up streak data
     if streak_count > 0: 
         yield LongStreak(
-            timestamp   = parse(match["match"]["date"]),
+            timestamp   = safe_dateparse(match["match"]["date"]),
             player_id   = player_id,
             player_name = player_name,
             battle_tag  = battle_tag,
@@ -229,10 +237,10 @@ def parse_player_matches(player, matches, cutoff_date:datetime.datetime = dateti
             won    = streak_won,
         )
         
-    if 'match' in locals(): 
-    #  Event 3            : Elo Peak
+    if highest_elo > 0 and lowest_elo > 0:
+        #  Event 3            : Elo Peak
         yield EloHigh(
-            timestamp   = parse(match["match"]["date"]),
+            timestamp   = safe_dateparse(match["match"]["date"]),
             player_id   = player_id,
             player_name = player_name,
             battle_tag  = battle_tag,
@@ -242,7 +250,7 @@ def parse_player_matches(player, matches, cutoff_date:datetime.datetime = dateti
         
         # Event 4: Elo Climb
         yield EloClimb(
-            timestamp   = parse(match["match"]["date"]),
+            timestamp   = safe_dateparse(match["match"]["date"]),
             player_id   = player_id,
             player_name = player_name,
             battle_tag  = battle_tag,
@@ -264,7 +272,7 @@ def parse_player_facts(search_term, cutoff_date:datetime.datetime = datetime.dat
     battle_tag = player["members"]["account"]["battleTag"]
     player_name = re.match(r"^(.*?)#", player["members"]["character"]["name"]).group(1)
     history = get_player_history(player_id)
-    
+    print(f"player_id={player_id}, battle_tag={battle_tag}, player_name={player_name}, history found: {len(history.get('matches',[]))}")
     for fact in parse_player_matches(player, history['matches'], cutoff_date=cutoff_date):
         yield fact
         
@@ -272,9 +280,9 @@ def parse_player_facts(search_term, cutoff_date:datetime.datetime = datetime.dat
         yield fact
 
 if __name__ == "__main__":
-    for i, event in enumerate(sorted(list(parse_player_facts("Pop101")), reverse=True)):
+    week_ago = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=7)
+    for i, event in enumerate(sorted(list(parse_player_facts("GiantDwarf#1120200", cutoff_date=week_ago)), reverse=True)):
         print(f"event {i}: {event}")
         print(f"score={event.impressive()},\traw_score={event.calc_impressive()}")
         print()
         print()
-    
